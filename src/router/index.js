5fce51da2d168e605d54725105f659f6a752bba1
JeonYeongtae <liminal.yeongtae@gmail.com>
2025-11-12 16:22:20 +0900

    :sparkles: Header, Navbar, DefaultLayout 디폴트 컴포넌트 구현 (기능 동작 확인을 위한 test 페이지 추가)

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../pages/home/HomeView.vue'
+
+import DefaultLayout from '@/components/layouts/DefaultLayout.vue'
+import homeRoutes from './home' // '페이지 묶음' import
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: HomeView,
+      component: DefaultLayout,
+      children: [
+        ...homeRoutes // '페이지 묶음'을 자식으로 포함
+      ]
     },
   ],
 })
